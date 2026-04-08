d7a7bbe130af00f144fe11ad75ff0d4154a7aa3a
Youssef Sayed <youssefsayed@example.com>
2026-04-08 14:36:00 +0200

    test

--- job-app/app/Http/Controllers/DashboardController.php
+++ job-app/app/Http/Controllers/DashboardController.php
@@ -11,46 +11,43 @@ class DashboardController extends Controller
      * Display a listing of the resource.
      */
     public function index(Request $request)
-{
-    $search = $request->search;
-    $filter = $request->filter;
+    {
+        $search = $request->search;
+        $filter = $request->filter;
 
-    $query = job_vacancy::query();
+        $query = job_vacancy::query();
 
 
-    // Search only
-    if ($search && !$filter) {
-        $query->where(function ($q) use ($search) {
-            $q->where('title', 'like', "%$search%")
-              ->orWhere('location', 'like', "%$search%")
-              ->orWhereHas('company', function ($c) use ($search) {
-                  $c->where('name', 'like', "%$search%");
-              });
-        });
-    }
-
-    // Filter only
-    if ($filter && !$search) {
-        $query->where('type', $filter);
-    }
-
-    // Search + Filter
-    if ($search && $filter) {
-        $query->where('type', $filter)
-              ->where(function ($q) use ($search) {
-                  $q->where('title', 'like', "%$search%")
+        // Search only
+        if ($search && !$filter) {
+            $query->where(function ($q) use ($search) {
+                $q->where('title', 'like', "%$search%")
                     ->orWhere('location', 'like', "%$search%")
                     ->orWhereHas('company', function ($c) use ($search) {
                         $c->where('name', 'like', "%$search%");
                     });
-              });
+            });
+        }
+
+        // Filter only
+        if ($filter && !$search) {
+            $query->where('type', $filter);
+        }
+
+        // Search + Filter
+        if ($search && $filter) {
+            $query->where('type', $filter)
+                ->where(function ($q) use ($search) {
+                    $q->where('title', 'like', "%$search%")
+                        ->orWhere('location', 'like', "%$search%")
+                        ->orWhereHas('company', function ($c) use ($search) {
+                            $c->where('name', 'like', "%$search%");
+                        });
+                });
+        }
+
+        $jobs = $query->latest()->paginate(10)->withQueryString();
+
+        return view('dashboard', compact('jobs'));
     }
-
-    $jobs = $query->latest()->paginate(10)->withQueryString();
-
-    return view('dashboard', compact('jobs'));
-}
-
-
-   
 }
