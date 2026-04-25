36e60f5363d2344f454b70645eb82f58246b1450
Youssef Sayed <youssefsayed@example.com>
2026-04-26 02:16:01 +0300

    الحمد لله

--- job-backoffice/database/seeders/DatabaseSeeder.php
+++ job-backoffice/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        
+
 
             User::firstOrCreate(
             ['email' => 'Admin@admin.com'],
@@ -111,4 +111,6 @@ class DatabaseSeeder extends Seeder
             }
         }
     }
+
+       
 }
